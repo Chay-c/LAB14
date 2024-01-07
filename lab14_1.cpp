b717b65e7f350820feb45146bf947123d4cf0d07
Chay <chayangkul_c@cmu.ac.th>
2024-01-07 18:01:31 +0700

    sdkfmb psfbmfspbms[of

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
-
 template <typename T>
 void insertionSort(T d[],int N){
-
+    int i;
+    for(i = 1; i < N; i++){
+        T current = d[i];
+        int j = i-1;
+        while(j >= 0 && d[j] < current){
+             d[j + 1] = d[j] ;
+            j--;
+        }
+        d[j+1] = current;
+        cout << "Pass " << i  << ":";
+        for(int i =0 ; i< N ; i++) cout << d[i] << " ";
+        cout << "\n";
+    }
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
